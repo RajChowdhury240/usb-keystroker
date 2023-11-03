9d1815f13f99516852b340820938e24982928119
rajchowdhury420 <rajchowdhury269@gmail.com>
2023-11-03 21:50:59 +0530

    Hack CTFs

---
+++ usb-keystroker.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from rich.progress import Progress
+
+DataFileName = "usb.dat"
+
+presses = []
+
+normalKeys = {"04":"a", "05":"b", "06":"c", "07":"d", "08":"e", "09":"f", "0a":"g", "0b":"h", "0c":"i", "0d":"j", "0e":"k", "0f":"l", "10":"m", "11":"n", "12":"o", "13":"p", "14":"q", "15":"r", "16":"s", "17":"t", "18":"u", "19":"v", "1a":"w", "1b":"x", "1c":"y", "1d":"z","1e":"1", "1f":"2", "20":"3", "21":"4", "22":"5", "23":"6","24":"7","25":"8","26":"9","27":"0","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>","2d":"-","2e":"=","2f":"[","30":"]","31":"\\","32":"<NON>","33":";","34":"'","35":"<GA>","36":",","37":".","38":"/","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
+
+shiftKeys = {"04":"A", "05":"B", "06":"C", "07":"D", "08":"E", "09":"F", "0a":"G", "0b":"H", "0c":"I", "0d":"J", "0e":"K", "0f":"L", "10":"M", "11":"N", "12":"O", "13":"P", "14":"Q", "15":"R", "16":"S", "17":"T", "18":"U", "19":"V", "1a":"W", "1b":"X", "1c":"Y", "1d":"Z","1e":"!", "1f":"@", "20":"#", "21":"$", "22":"%", "23":"^","24":"&","25":"*","26":"(","27":")","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>","2d":"_","2e":"+","2f":"{","30":"}","31":"|","32":"<NON>","33":":","34":"\"","35":"<GA>","36":"<","37":">","38":"?","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage : ")
+        print("python usb-keystroker.py data.pcap")
+        exit(1)
+
+    pcapFilePath = sys.argv[1]
+    
+    with Progress() as progress:
+        task = progress.add_task("[cyan]Processing...", total=100)
+
+        os.system("tshark -r %s -T fields -e usb.capdata 'usb.data_len == 8' > %s" % (pcapFilePath, DataFileName))
+
+        with open(DataFileName, "r") as f:
+            lines = list(f)
+            progress.update(task, completed=20)
+
+        for idx, line in enumerate(lines):
+            presses.append(line[0:-1])
+            progress.update(task, completed=20 + int((idx / len(lines)) * 60))
+
+    result = ""
+    for press in presses:
+        if press == '':
+            continue
+        if ':' in press:
+            Bytes = press.split(":")
+        else:
+            Bytes = [press[i:i+2] for i in range(0, len(press), 2)]
+        if Bytes[0] == "00":
+            if Bytes[2] != "00" and normalKeys.get(Bytes[2]):
+                result += normalKeys[Bytes[2]]
+        elif int(Bytes[0],16) & 0b10 or int(Bytes[0],16) & 0b100000:
+            if Bytes[2] != "00" and normalKeys.get(Bytes[2]):
+                result += shiftKeys[Bytes[2]]
+        else:
+            print("[-] Unknow Key : %s" % (Bytes[0]))
+    print("[+] Found : %s" % (result))
+
+    os.system("rm ./%s" % (DataFileName))
+
+
+if __name__ == "__main__":
+    main()
